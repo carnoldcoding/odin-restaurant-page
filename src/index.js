6e3a09f9b70c643c971e16deac01c28a9c0ac481
carnoldcoding <carnoldcoding@gmail.com>
2022-10-04 17:16:57 -0400

    All setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-console.log("Working");
+import "./styles.scss"
+
+console.log("Working On it");
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,29 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  mode: "development",
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  watch: true,
+  watchOptions:{
+    ignored: /node_modules/,
+    aggregateTimeout: 2000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
   },
 };
